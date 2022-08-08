e3166fbee7a95ad61c1ec7c844df4f5543cc4bfd
rupansh <rupanshsekar@hotmail.com>
2022-08-08 11:06:08 +0000

    Build: (51d7786) export notifier and exchange rate deps

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,9 @@
 export * from "./chains";
 export * from "./chains/evm";
 export * from "./external/sc-verify";
+export * from "./external/exchange-rate";
+export * from "./external/notifier";
+export * from "./external/deps";
 export * from "./multi-bridge";
 export { ChainInfo } from "./multi-bridge/chain-info";
 export { ChainNonce } from "./multi-bridge/meta";
