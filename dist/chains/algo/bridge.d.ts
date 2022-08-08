02dc7dcfb81dd0c41e7acabf090bcc03924478ea
rupansh <rupanshsekar@hotmail.com>
2022-08-08 13:00:51 +0000

    Build: (27f8e2b) add checks to see if its safe to receive nft

--- dist/chains/algo/bridge.d.ts
+++ dist/chains/algo/bridge.d.ts
@@ -1,7 +1,7 @@
-import { FullBridgeChain } from "..";
+import { Erc20TransferChecks, FullBridgeChain } from "..";
 import algosdk from "algosdk";
 declare type AlgoSigenr = algosdk.Account;
-export declare type AlgoBridgeChain = FullBridgeChain<AlgoSigenr, number, bigint, string, algosdk.Address>;
+export declare type AlgoBridgeChain = FullBridgeChain<AlgoSigenr, number, bigint, string, algosdk.Address> & Erc20TransferChecks<number, algosdk.Address>;
 export declare type AlgoParams = {
     algod: algosdk.Algodv2;
     indexer: algosdk.Indexer;
--- dist/chains/index.d.ts
+++ dist/chains/index.d.ts
@@ -6,12 +6,16 @@ export declare type EstimateTxFee<A> = {
     estimateTransferNative: () => Promise<A>;
     estimateTransferWrapped: () => Promise<A>;
 };
+export declare type Erc20TransferChecks<T, Addr> = {
+    preReceiveForeignCheck(token: T, receiver: Addr): Promise<string | undefined>;
+    preReceiveNativeCheck(token: T, receiver: Addr): Promise<string | undefined>;
+};
 export declare type Erc20BridgeChain<S, T, A, Txn> = {
     preTransfer(sender: S, token: T, amt: A): Promise<Txn | undefined>;
     transferNative(sender: S, nativeToken: T, chainNonce: number, amt: A, to: string, txFee: A): Promise<Txn>;
     transferWrapped(sender: S, wToken: T, chainNonce: number, amt: A, to: string, txFee: A): Promise<Txn>;
 };
-export declare type FullBridgeChain<S, T, A, Txn, Addr> = Erc20BridgeChain<S, T, A, Txn> & EstimateTxFee<A> & Erc20Utils<T, A, Addr>;
+export declare type FullBridgeChain<S, T, A, Txn, Addr> = Erc20BridgeChain<S, T, A, Txn> & EstimateTxFee<A> & Erc20Utils<T, A, Addr> & Partial<Erc20TransferChecks<T, Addr>>;
 export declare type BridgeChainMapper<T, A, Txn, Addr> = {
     txnToDomain(txn: Txn): string;
     addrFromDomain(addr: string): Addr;
